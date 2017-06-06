e2ee3bce49fb276bf49d635c2552f25463f9c777
Nícolas Oreques de Araujo <nodaraujo@inf.ufpel.edu.br>
2017-06-06 13:42:58 -0300

    Adicionado campos do formulário de inscrição ao model.Item

--- src/model/Item.java
+++ src/model/Item.java
@@ -16,18 +16,86 @@ public class Item implements Comparable<Item>{
     private String name;
     private int year;
     private String status;
+    //Measures
+    private float lenght;
+    private float height;
+    private float width;
+    private float thickness;
+    private float depth;
+    private float outerCircumference;
+    private float innerCircumference;
+    private float weight;
+    //Extra info
+    private String author;
+    private String conservationState;
+    private String historicalContext;
+    private String biography;
+    private String description;
+    private java.sql.Date aquisitionDate;
 
     private TreeMap<Date, Movimentation> movimentationsTreeMap;
 
 
-    Item(String ID, String name, int year, String origin, String destination) {
+    Item(String ID, String name, int year, String origin, String destination, float weight, float width, float height, float thickness, String description, String author, java.sql.Date aquisitionDate) {
 
         /* -- Cria item --  */
         this.ID = ID;
         this.name = name;
         this.year = year;
         this.movimentationsTreeMap = new TreeMap<>();
+        this.weight = weight;
+        this.width = width;
+        this.height = height;
+        this.thickness = thickness;
+        this.description = description;
+        this.author = author;
+        this.aquisitionDate = aquisitionDate;
+        /* -- Cria Movimentação de entrada -- */
+        Movimentation admission = new AdmissionMovimentation(new Date(java.lang.System.currentTimeMillis()), System.getActiveUser().getCpf(), origin, destination);
+        movimentationsTreeMap.put(admission.getTimestamp(), admission);
+        this.status = Utils.AT_STORAGE;
 
+    }
+    Item(String ID, String name, int year, String origin, String destination, float weight, float width, float height, float thickness, String description, float outerCircumference, float innerCircumference, float depth, String conservationState, String author, String historicalContext, java.sql.Date aquisitionDate) {
+
+        /* -- Cria item --  */
+        this.ID = ID;
+        this.name = name;
+        this.year = year;
+        this.movimentationsTreeMap = new TreeMap<>();
+        this.weight = weight;
+        this.width = width;
+        this.height = height;
+        this.thickness = thickness;
+        this.description = description;
+        this.author = author;
+        this.innerCircumference = innerCircumference;
+        this.outerCircumference = outerCircumference;
+        this.depth = depth;
+        this.aquisitionDate = aquisitionDate;
+        /* -- Cria Movimentação de entrada -- */
+        Movimentation admission = new AdmissionMovimentation(new Date(java.lang.System.currentTimeMillis()), System.getActiveUser().getCpf(), origin, destination);
+        movimentationsTreeMap.put(admission.getTimestamp(), admission);
+        this.status = Utils.AT_STORAGE;
+
+    }
+
+    Item(String ID, String name, int year, String origin, String destination, float weight, float width, float height, float thickness, String description, String author, float outerCircumference, float innerCircumference, float depth) {
+
+        /* -- Cria item --  */
+        this.ID = ID;
+        this.name = name;
+        this.year = year;
+        this.movimentationsTreeMap = new TreeMap<>();
+        this.weight = weight;
+        this.width = width;
+        this.height = height;
+        this.thickness = thickness;
+        this.description = description;
+        this.author = author;
+        this.innerCircumference = innerCircumference;
+        this.outerCircumference = outerCircumference;
+        this.depth = depth;
         /* -- Cria Movimentação de entrada -- */
         Movimentation admission = new AdmissionMovimentation(new Date(java.lang.System.currentTimeMillis()), System.getActiveUser().getCpf(), origin, destination);
         movimentationsTreeMap.put(admission.getTimestamp(), admission);
@@ -71,6 +139,118 @@ public class Item implements Comparable<Item>{
 
     public TreeMap<Date, Movimentation> getMovimentationsTreeMap() {
         return movimentationsTreeMap;
+    }
+
+    public float getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(float lenght) {
+        this.lenght = lenght;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getThickness() {
+        return thickness;
+    }
+
+    public void setThickness(float thickness) {
+        this.thickness = thickness;
+    }
+
+    public float getDepth() {
+        return depth;
+    }
+
+    public void setDepth(float depth) {
+        this.depth = depth;
+    }
+
+    public float getOuterCircumference() {
+        return outerCircumference;
+    }
+
+    public void setOuterCircumference(float outerCircumference) {
+        this.outerCircumference = outerCircumference;
+    }
+
+    public float getInnerCircumference() {
+        return innerCircumference;
+    }
+
+    public void setInnerCircumference(float innerCircumference) {
+        this.innerCircumference = innerCircumference;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getConservationState() {
+        return conservationState;
+    }
+
+    public void setConservationState(String conservationState) {
+        this.conservationState = conservationState;
+    }
+
+    public String getHistoricalContext() {
+        return historicalContext;
+    }
+
+    public void setHistoricalContext(String historicalContext) {
+        this.historicalContext = historicalContext;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getAquisitionDate() {
+        return aquisitionDate;
+    }
+
+    public void setAquisitionDate(Date aquisitionDate) {
+        this.aquisitionDate = aquisitionDate;
     }
 
     /* -- Movimentation -- */
