41461fdd9a2493cf00b0351de30f97374a05717e
Kellerson Kurtz <kkurtz@inf.ufpel.edu.br>
2017-06-10 07:24:22 -0300

    Adicionar movimentações "implementado"
    Os métodos de adicionar movimentação precisam ser revistos, faltam alguns parametros e todos pedem "coleção", sendo que o item nao enxerga a sua coleção. A interface pede pra pessoa digitar seu CPF, ta errado, né? O certo é pegar o user ativo. E falta campos na interface tbm!!

--- src/ds/AddMovimentationController.java
+++ src/ds/AddMovimentationController.java
@@ -6,8 +6,23 @@
 package ds;
 
 import java.net.URL;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import model.PutToExpositionMovimentation;
 
 /**
  * FXML Controller class
@@ -15,13 +30,66 @@ import javafx.fxml.Initializable;
  * @author Usuário
  */
 public class AddMovimentationController implements Initializable {
+    
+    model.Item item;
 
+    @FXML
+    private ChoiceBox MovimentationTypeChoiceBox;
+    
+    @FXML
+    private Button AddMovimentationButton;
+    
+    @FXML
+    private TextField Origin;
+    
+    @FXML
+    private TextField Destination;
+    
+    @FXML
+    private TextField AuthorCPF;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        ArrayList<String> collections;
+        collections = new ArrayList<>();
+        collections.add("Empréstimo");
+        collections.add("Restauração");
+        collections.add("Armazenamento");
+        collections.add("Baixa");
+        collections.add("Exposição");
+        ObservableList<String> list = FXCollections.observableArrayList(collections);
+        MovimentationTypeChoiceBox.setItems(list);
+        
+        
+        AddMovimentationButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    if(MovimentationTypeChoiceBox.getValue().equals("Exposição")){                      
+                        //model.System.exposeItem("Picasso", item.getID(), null, Destination.getText());
+                    }
+                    if(MovimentationTypeChoiceBox.getValue().equals("Empréstimo")){                      
+                        //model.System.loanItem("Picasso", item.getID(), null, null, Destination.getText());
+                    }
+                    if(MovimentationTypeChoiceBox.getValue().equals("Restauração")){                                        
+                        //model.System.restoreItem("Picasso", item.getID(), null, Destination.getText());
+                    }
+                    if(MovimentationTypeChoiceBox.getValue().equals("Armazenamento")){                                        
+                        //model.System.storeItem("Picasso", item.getID(), null, Destination.getText());
+                    }
+                    if(MovimentationTypeChoiceBox.getValue().equals("Baixa")){                                        
+                        //model.System.dischargeItem("Picasso", item.getID(), null);
+                    }
+                }
+            }   
+        );
+    }
+
+    public void setItem(model.Item item){
+        this.item=item;
+        
+    }
     
 }
--- src/ds/MovimentationController.java
+++ src/ds/MovimentationController.java
@@ -5,15 +5,25 @@
  */
 package ds;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -22,19 +32,50 @@ import javafx.scene.layout.AnchorPane;
  */
 public class MovimentationController implements Initializable {
     
+    model.Item item;
+    
     @FXML
     private Accordion Accordion; 
+    
+    @FXML
+    private Button AddMovimentationPane;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        AddMovimentationPane.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //inicio
+                Stage stage; 
+                Parent root = null;
+
+                //get reference to the button's stage         
+                stage=(Stage) AddMovimentationPane.getScene().getWindow();
+                try {
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AddMovimentation.fxml"));
+                    AddMovimentationController addMovimentationController = new AddMovimentationController();
+                    fxmlLoader.setController(addMovimentationController);
+                    root = (Parent) fxmlLoader.load();
+                    stage.setScene(new Scene(root));  
+                    stage.show();
+                    addMovimentationController.setItem(item);
+                } catch (IOException ex) {
+                    Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+               
+                //fim
+            }
+        }
+        );
+
     }
 
     @FXML
     public void showMovimentations(model.Item item){
+        this.item = item;
         AnchorPane newPanelContent = new AnchorPane();
         ArrayList<model.Movimentation> movimentations = new ArrayList<>(item.getMovimentationsTreeMap().values());
         for(model.Movimentation movimentation : movimentations){
