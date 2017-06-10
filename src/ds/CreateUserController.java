77eda30406067b9aacfc8de1abf712e4affe4031
Kellerson Kurtz <kkurtz@inf.ufpel.edu.br>
2017-06-10 07:57:53 -0300

    Cadastro de Usuário Funcionando

--- src/ds/CreateUserController.java
+++ src/ds/CreateUserController.java
@@ -7,7 +7,13 @@ package ds;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,13 +21,60 @@ import javafx.fxml.Initializable;
  * @author Usuário
  */
 public class CreateUserController implements Initializable {
-
+    
+    @FXML
+    private RadioButton ResearcherButton;
+    
+    @FXML
+    private RadioButton TecnicianButton;
+    
+    @FXML
+    private RadioButton DirectorButton;
+    
+    @FXML
+    private Button AddUserButton;
+    
+    @FXML
+    private TextField Name;
+    
+    @FXML
+    private TextField Password;
+    
+    @FXML
+    private TextField CPF;
+    
+    @FXML
+    private TextField Email;
+    
+    @FXML
+    private TextField Phone;
+    
+    @FXML
+    private TextField UniversityRegistration;
+    
+    @FXML
+    private TextField MuseumID;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        AddUserButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(ResearcherButton.isSelected()){
+                    model.System.addPerson(Name.getText(), CPF.getText(), Password.getText(), Email.getText(), Integer.parseInt(UniversityRegistration.getText()));
+                }
+                if(TecnicianButton.isSelected()){
+                    model.System.addPerson(Name.getText(), CPF.getText(), Password.getText(), Email.getText(), MuseumID.getText());
+                }
+                if(ResearcherButton.isSelected()){
+                    model.System.addPerson(Name.getText(), CPF.getText(), Password.getText(), Email.getText());
+                }
+                
+            }
+        });
     }    
     
 }
