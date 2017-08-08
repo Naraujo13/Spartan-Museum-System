a50e113c39a50acfc40a8740913ae9ed6d43af8b
Felipe Luzzardi da Rosa <fldrosa@inf.ufpel.edu.br>
2017-08-07 21:50:18 -0300

    Tela de coleções implementada.

--- Web/js/changeDiv.js
+++ Web/js/changeDiv.js
@@ -1,9 +1,17 @@
 $( "#Home" ).click(function() {
   $("#Main-Usuarios").hide("liniear");
+  $("#Main-Colecoes").hide("liniear");
   $("#MainScreen-BemVindo").show("liniear");
 });
 
 $( "#usersLink" ).click(function() {
   $("#MainScreen-BemVindo").hide("liniear");
+  $("#Main-Colecoes").hide("liniear");
   $("#Main-Usuarios").show("liniear");
+});
+
+$( "#collectionsLink" ).click(function() {
+  $("#MainScreen-BemVindo").hide("liniear");
+  $("#Main-Usuarios").hide("liniear");
+  $("#Main-Colecoes").show("liniear");
 });
