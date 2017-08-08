0cf73d80962dfd30ab964dcab8410bc70e297746
Felipe Luzzardi da Rosa <fldrosa@inf.ufpel.edu.br>
2017-08-07 23:29:02 -0300

    Tela itens implementada.

--- Web/js/changeDiv.js
+++ Web/js/changeDiv.js
@@ -1,17 +1,27 @@
 $( "#Home" ).click(function() {
   $("#Main-Usuarios").hide("liniear");
   $("#Main-Colecoes").hide("liniear");
+  $("#Main-Itens").hide("liniear");
   $("#MainScreen-BemVindo").show("liniear");
 });
 
 $( "#usersLink" ).click(function() {
   $("#MainScreen-BemVindo").hide("liniear");
   $("#Main-Colecoes").hide("liniear");
+  $("#Main-Itens").hide("liniear");
   $("#Main-Usuarios").show("liniear");
 });
 
 $( "#collectionsLink" ).click(function() {
   $("#MainScreen-BemVindo").hide("liniear");
   $("#Main-Usuarios").hide("liniear");
-  $("#Main-Colecoes").show("liniear");
+  $("#Main-Itens").hide("liniear");
+  $("#Main-Colecoes").show("liniear")
+});
+
+$( "#itensLink" ).click(function() {
+  $("#MainScreen-BemVindo").hide("liniear");
+  $("#Main-Usuarios").hide("liniear");
+  $("#Main-Colecoes").hide("liniear");
+  $("#Main-Itens").show("liniear");
 });
