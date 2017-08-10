a5734564f5e5e82042290e73c5bf3604417c5d02
Kellerson Kurtz <kkurtz@inf.ufpel.edu.br>
2017-08-10 00:22:58 -0300

    Página principal coleções, usuários e itens

--- Web/php/spartan/public/database/migrations/2014_10_12_000000_create_users_table.php
+++ Web/php/spartan/public/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('password');
-            $table->integer('function');
             $table->rememberToken();
             $table->timestamps();
         });
